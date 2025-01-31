86914b771eca9421bf9e29ad8b6823937bfb6000
NghiaLio <nghianak31@gmail.com>
2025-01-31 11:19:05 +0700

    fix state manageFolder in drawer

--- lib/Views/Components/Drawer.dart
+++ lib/Views/Components/Drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Bloc/Events/ManageFolderCubit.dart';
+import 'package:flutter_application_1/Bloc/States/ManageFolderState.dart';
 import 'package:flutter_application_1/Bloc/States/NavigationState.dart';
 import 'package:flutter_application_1/Config/configColor.dart';
 import 'package:flutter_application_1/Models/Data/folder.dart';
@@ -21,9 +22,10 @@ class HomeDrawer extends StatefulWidget {
 class _HomeDrawerState extends State<HomeDrawer> {
   late bool isOpenFolder;
   late bool backGroundItemFunction;
-  
-  void tapToManage(){
-    Navigator.push(context, MaterialPageRoute(builder: (c)=>const Managefolder()));
+
+  void tapToManage() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (c) => const Managefolder()));
   }
 
   void openMoreFolder() {
@@ -33,15 +35,20 @@ class _HomeDrawerState extends State<HomeDrawer> {
     });
   }
 
-  void tapToNoteOfFolder(folder dataFolder){
-    Navigator.push(context, MaterialPageRoute(builder: (c)=> NoteOfFolder(dataFolder:dataFolder ,)));
+  void tapToNoteOfFolder(folder dataFolder) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (c) => NoteOfFolder(
+                  dataFolder: dataFolder,
+                )));
   }
 
   @override
   void initState() {
-    if(widget.currentIndexPage! == 4){
+    if (widget.currentIndexPage! == 4) {
       isOpenFolder = true;
-    }else{
+    } else {
       isOpenFolder = false;
     }
 
@@ -50,14 +57,14 @@ class _HomeDrawerState extends State<HomeDrawer> {
 
   @override
   Widget build(BuildContext context) {
-    List<folder> listFolder = context.read<manageFolderCubit>().listFolder ?? [];
+
     final size = MediaQuery.of(context).size;
     return Container(
       height: size.height * 0.95,
       width: size.width * 0.8,
-      decoration:  BoxDecoration(
+      decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.primary,
-          borderRadius:  const BorderRadius.only(
+          borderRadius: const BorderRadius.only(
             topRight: Radius.circular(15),
             bottomRight: Radius.circular(15),
           )),
@@ -70,7 +77,10 @@ class _HomeDrawerState extends State<HomeDrawer> {
               padding: const EdgeInsets.only(right: 20, top: 20),
               child: IconButton(
                   onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (c)=>const settingScreen()));
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (c) => const settingScreen()));
                   },
                   icon: Icon(
                     Icons.settings,
@@ -85,19 +95,23 @@ class _HomeDrawerState extends State<HomeDrawer> {
             ItemFunction(Icons.description_outlined, "Tất cả ghi chú", () {
               context.read<NavigationCubit>().nextScreen(0);
               context.read<Opendrawer>().getClose();
-            }, context.read<NavigationCubit>().backGroundItemFunction[0],Theme.of(context).colorScheme.onSurface),
+            }, context.read<NavigationCubit>().backGroundItemFunction[0],
+                Theme.of(context).colorScheme.onSurface),
             ItemFunction(Icons.lock, "Ghi chú bị khóa", () {
               context.read<NavigationCubit>().nextScreen(1);
               context.read<Opendrawer>().getClose();
-            }, context.read<NavigationCubit>().backGroundItemFunction[1],Theme.of(context).colorScheme.onSurface),
+            }, context.read<NavigationCubit>().backGroundItemFunction[1],
+                Theme.of(context).colorScheme.onSurface),
             ItemFunction(Icons.star, "Mục yêu thích", () {
               context.read<NavigationCubit>().nextScreen(2);
               context.read<Opendrawer>().getClose();
-            }, context.read<NavigationCubit>().backGroundItemFunction[2],Theme.of(context).colorScheme.onSurface),
+            }, context.read<NavigationCubit>().backGroundItemFunction[2],
+                Theme.of(context).colorScheme.onSurface),
             ItemFunction(Icons.delete, "Thùng rác", () {
               context.read<NavigationCubit>().nextScreen(3);
               context.read<Opendrawer>().getClose();
-            }, context.read<NavigationCubit>().backGroundItemFunction[3],Theme.of(context).colorScheme.onSurface),
+            }, context.read<NavigationCubit>().backGroundItemFunction[3],
+                Theme.of(context).colorScheme.onSurface),
 
             Divider(
               thickness: 0.5,
@@ -111,20 +125,37 @@ class _HomeDrawerState extends State<HomeDrawer> {
                 "Thư mục",
                 openMoreFolder,
                 context.read<NavigationCubit>().backGroundItemFunction[4],
-                Theme.of(context).colorScheme.onSurface
-            ),
+                Theme.of(context).colorScheme.onSurface),
 
             //more folder
             isOpenFolder
-                ? Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Column(
-                      children: [
-                        for(int i=0; i< listFolder.length; i++)
-                          ItemFunction(Icons.folder_open, listFolder[i].name, ()=>tapToNoteOfFolder(listFolder[i]), false, configColor.rgbaToColor(listFolder[i].color))
-                      ],
-                    ),
-                  )
+                ? BlocBuilder<manageFolderCubit,manageFolderState>(
+                    builder: (context,state){
+                      if(state is loadedManage){
+                        List<folder> listFolder = state.listFolder ?? [];
+                        return Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Column(
+                            children: [
+                              for (int i = 0; i < listFolder.length; i++)
+                                ItemFunction(
+                                    Icons.folder_open,
+                                    listFolder[i].name,
+                                        () => tapToNoteOfFolder(listFolder[i]),
+                                    false,
+                                    configColor.rgbaToColor(listFolder[i].color)
+                                )
+                            ],
+                          ),
+                        );
+                      }else{
+                        return Text(
+                          '.............',
+                          style: TextStyle(fontSize: 16, color: Theme.of(context).colorScheme.surface),
+                        );
+                      }
+                    }
+                )
                 : Container(),
             const SizedBox(
               height: 20,
@@ -140,12 +171,18 @@ class _HomeDrawerState extends State<HomeDrawer> {
                     height: size.height * 0.05,
                     width: size.width * 0.6,
                     decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.onSurface.withOpacity(0.4),
+                        color: Theme.of(context)
+                            .colorScheme
+                            .onSurface
+                            .withOpacity(0.4),
                         borderRadius:
                             const BorderRadius.all(Radius.circular(50))),
                     child: Text(
                       'Quản lý thư mục',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600,color:Theme.of(context).colorScheme.surface),
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Theme.of(context).colorScheme.surface),
                     ),
                   ),
                 ),
@@ -157,13 +194,15 @@ class _HomeDrawerState extends State<HomeDrawer> {
     );
   }
 
-  Widget ItemFunction(
-      IconData icon, String title, Function()? onTap, bool isBackground,Color colorIcon) {
+  Widget ItemFunction(IconData icon, String title, Function()? onTap,
+      bool isBackground, Color colorIcon) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10.0),
       child: Container(
         decoration: BoxDecoration(
-            color: isBackground ? Theme.of(context).colorScheme.onSurface.withOpacity(0.4) : Theme.of(context).colorScheme.primary,
+            color: isBackground
+                ? Theme.of(context).colorScheme.onSurface.withOpacity(0.4)
+                : Theme.of(context).colorScheme.primary,
             borderRadius: const BorderRadius.all(Radius.circular(10))),
         child: ListTile(
           onTap: onTap,
@@ -174,7 +213,10 @@ class _HomeDrawerState extends State<HomeDrawer> {
           ),
           title: Text(
             title,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400, color:Theme.of(context).colorScheme.surface ),
+            style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+                color: Theme.of(context).colorScheme.surface),
           ),
         ),
       ),
